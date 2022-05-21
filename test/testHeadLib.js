c89a3b915858f4bb58922e08ed655b73bac501f2
Pritam Bhuiya <99480050+pritambhuiya@users.noreply.github.com>
2022-05-21 14:19:03 +0530

    Wrote test for head function

--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -1,4 +1,4 @@
-const { contentsUpto, headMain } = require('../src/headLib.js');
+const { contentsUpto, head, headMain } = require('../src/headLib.js');
 
 const assert = require('assert');
 
@@ -16,6 +16,25 @@ describe('contentsUpto', () => {
   it('Should return all elements when upto is greater then elements', () => {
     assert.deepStrictEqual(contentsUpto(['a'], 2), ['a']);
     assert.deepStrictEqual(contentsUpto(['a', 'b'], 3), ['a', 'b']);
+  });
+});
+
+describe('head', () => {
+  it('Should work on overridden -c', () => {
+    assert.deepStrictEqual(head(['a\nb\nc'], '', 1), ['a']);
+  });
+
+  it('Should work on overridden -n', () => {
+    assert.deepStrictEqual(head(['a\nb\nc'], '\n', 1), ['a']);
+  });
+
+  it('Should return all contets when only contents is given', () => {
+    assert.deepStrictEqual(head(['a\nb\nc'], '\n', 10), ['a\nb\nc']);
+  });
+
+  it('Should work for multi-contents', () => {
+    assert.deepStrictEqual(head(['a\nb\nc', 'a\nb'], '\n', 10),
+      ['a\nb\nc', 'a\nb']);
   });
 });
 
